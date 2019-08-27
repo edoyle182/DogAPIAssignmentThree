bca25a255c1edc3d212115742c52ebed7fbd720c
Sarah Kaplan <sarahkaplan@Sarahs-MBP-2.lan>
2019-08-27 18:11:15 -0400

    Added script.js file and made fixes to index.html & style.css

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function getDogImage(breed) {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+
+  if (responseJson.message == "Breed not found") {
+    alert('That breed wasn\'t found, please try another.');
+  } else {
+    // replace existing img with new one
+    $('.results').html(`<h2>Look at this pup!</h2>`);
+
+    let splitUrl = responseJson.message.split("/");
+    let breedName = splitUrl[4];
+    $('.results').append(`<h3>${breedName}</h3>`);
+
+    $('.results').append(
+      `<img src="${responseJson.message}" class="results-img" width="200" height="auto">`
+    );
+    // display results section
+    $('.results').removeClass('hidden');
+  }
+}
+
+function watchUserInput() {
+  $('form').submit(event => {
+    event.preventDefault();
+    let dogBreed = $('input[name="dogBreed"]').val();
+    getDogImage(dogBreed);
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchUserInput();
+});
